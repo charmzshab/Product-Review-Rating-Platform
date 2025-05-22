7d0de92439ec70840ea20f6955c03702b818ec03
Lukyamuzi Shaban <kmharouna@gmail.com>
2025-05-22 14:43:31 -0500

    server configurations

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,7 +3,7 @@ import { MONGO_URI } from "./env";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI);
+    const conn = await mongoose.connect(MONGO_URI as string);
     console.log(`Connection to DB established at ${conn.connection.host}`);
   } catch (e: any) {
     console.warn("Error connecting to DB: ", e);
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,2 +1,3 @@
-export const MONGO_URI = "mongodb://localhost:27017/myapp";
+export const MONGO_URI_Local = "mongodb://localhost:27017/myapp";
 export const PORT = 4000;
+export const MONGO_URI ="mongodb+srv://kshabanlukyamuzi:HSRcly3WMirhB0qD@cluster0.9ucw2nx.mongodb.net/myapp?retryWrites=true&w=majority";
